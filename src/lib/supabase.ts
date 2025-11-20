28c91406b65befda53b36277b7abf659054e9742
nextwaveaisuite <nextwaveaisuite@gmail.com>
2025-11-20 13:49:32 +1000

    supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient as create } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+export function createClient() {
+  return create(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_KEY!
+  );
+}
